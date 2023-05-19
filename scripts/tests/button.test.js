3a02736f51a1df1a32b1d5fe77023eb09f4f5bcf
Emile <bvcxzq1@gmail.com>
2023-05-19 10:13:47 +0000

    Add HTML, CSS and JS files for game

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const buttonClick = require("../button")
+
+beforeEach (() => {
+    let fs = require("fs");
+    let fileContent = fs.readFileSync("index.html", "utf-8")
+    document.open();
+    document.write(fileContent);
+    document.close();
+
+})
+
+describe("DOM tests", () => {
+    test("expects p contect to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked")
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    })
+});
